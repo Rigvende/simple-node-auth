6f15895c4f5a39a0e6ab636e54e9520454e87b73
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-11-24 15:25:31 +0300

    login bug fixed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const handler = require('../responseCodesHandler.js');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 exports.getMainPage = (req, res) =>
     res.send(`<h1>Hello from authentication-trainee!</h1>`);
@@ -13,8 +14,12 @@ exports.login = async (req, res) => {
             handler.send400(res, errors, "Incorrect login data");
         } else {
             const { email, password } = req.body;
-            const user = await User.findOne({ where: { email, password } });
+            const user = await User.findOne({ where: { email } });
             if (user) {
+                const isMatch = await bcrypt.compare(password, user.password);
+                if (!isMatch) {
+                    handler.send401(res);
+                }
                 handler.send200(res, { message: `User ${user.id} successfully logged in` });
             } else {
                 handler.send401(res);
