a4590067b9104f126292f05013011d1a32540170
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-11-30 19:12:03 +0300

    herrokuuu

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -14,12 +14,12 @@ const { DATABASE_URL } = process.env;
 
 const sequelize = new Sequelize(DATABASE_URL, {
     dialect: "postgres",
-    dialectOptions: { ssl: {
+    dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
-    }},
+    },
     query: { raw: true },
 });
 
