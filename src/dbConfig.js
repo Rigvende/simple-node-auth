46684a9210374ed18c884aa68251283c47c8afcf
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-11-30 18:23:28 +0300

    herrrroku

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -2,23 +2,23 @@ const Sequelize = require("sequelize");
 
 //for localhost:
 
-const {DB_NAME, DB_LOGIN, DB_PASSWORD, DB_DIALECT} = process.env;
-const sequelize = new Sequelize(DB_NAME, DB_LOGIN, DB_PASSWORD, {
-dialect: DB_DIALECT,
-query: {raw: true}
-}); 
+// const {DB_NAME, DB_LOGIN, DB_PASSWORD, DB_DIALECT} = process.env;
+// const sequelize = new Sequelize(DB_NAME, DB_LOGIN, DB_PASSWORD, {
+// dialect: DB_DIALECT,
+// query: {raw: true}
+// }); 
 
 //for heroku:
 
-// const { DATABASE_URL } = process.env;
+const { DATABASE_URL } = process.env;
 
-// const sequelize = new Sequelize(DATABASE_URL, {
-//   dialect: "postgres",
-//   ssl: {
-//     require: true,
-//     rejectUnauthorized: false 
-//   },
-//   query: { raw: true },
-// });
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: "postgres",
+  ssl: {
+    require: true,
+    rejectUnauthorized: false 
+  },
+  query: { raw: true },
+});
 
 module.exports = sequelize;
