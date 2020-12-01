896349610977a555018d8c5beaa8c784f683e6d4
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-12-01 11:24:23 +0300

    heroku

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -7,7 +7,7 @@ module.exports = async (req, res, next) => {
     }
 
     try {
-        const token = req.headers['Authorization'].split(' ')[1];
+        const token = req.headers.authorization.split(' ')[1];
         if (!token) {
             throw new Error();
         }
