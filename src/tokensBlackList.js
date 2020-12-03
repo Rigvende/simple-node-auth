948f8bf8aeff27a1d4346aa985e423d6663096c2
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-12-03 15:11:37 +0300

    once more refactoring

--- src/tokensBlackList.js
+++ src/tokensBlackList.js
@@ -2,8 +2,8 @@ const redis = require('redis');
 const { logger } = require('./logger.js');
 const bluebird = require('bluebird');
 
-const client = redis.createClient(); //for localhost
-// const client = redis.createClient(process.env.REDIS_URL); //for heroku
+// const client = redis.createClient(); //for localhost
+const client = redis.createClient(process.env.REDIS_URL); //for heroku
 
 client.on("connect", () => {
     logger.info("Redis is now connected");
@@ -11,27 +11,12 @@ client.on("connect", () => {
 });
 client.on("error", err => logger.error(`Redis error occurred: ${err}`));
 
-// exports.invalidateToken = (id, token) =>
-//     client.set(id, token, err => {
-//         if (err) {
-//             throw err;
-//         }
-//     });
-
-exports.invalidateToken = (id, token) =>
+exports.invalidateToken = async (id, token) =>
     new Promise((resolve, reject) =>
         client.set(id, token, err =>
             err ? reject(err) : resolve()));
 
-exports.checkToken = async (id, token) => {
-    return client.getAsync(id)
-        .then(data => {
-            if (data === token) {
-                return false;
-            }
-            return true;
-        })
-        .catch(err => {
-            return false;
-        });
-};
+exports.checkToken = async (id, token) =>
+    client.getAsync(id)
+        .then(data => data !== token)
+        .catch(err => false);
