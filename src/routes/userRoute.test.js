b88817d8688187793d2d60300d566b588f9201e5
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-12-08 12:57:38 +0300

    second test with token

--- src/routes/userRoute.test.js
+++ src/routes/userRoute.test.js
@@ -2,7 +2,8 @@
 require('dotenv').config();
 const express = require("express");
 const userRoute = require("./userRoute");
-const request = require("supertest"); // 
+const authRoute = require("./authRoute");
+const request = require("supertest");
 const { StatusCodes } = require('http-status-codes');
 const { addCustomResponses } = require('../middleware/responseCodes.middleware');
 
@@ -10,20 +11,41 @@ const app = express();
 app.use(express.json({ extended: true }));
 app.use(addCustomResponses);
 app.use("/users", userRoute);
+app.use("/", authRoute);
 const agent = request.agent(app);
 
 jest.setTimeout(10000);
 
 describe("Testing userRoute", () => {
-    it("POST /users - success", async () => {
+    let token;
+
+    beforeAll(async () => {
+        let login = {
+            'email': "11111@gmail.com",
+            'password': "000"
+        }
+        auth = await agent.post("/auth").send(login);
+        token = JSON.parse(auth.text).token;
+        agent.set('Authorization', `Bearer ${token}`);
+    });
+
+    it("POST /users - already existed", async () => {
         let userObj = {
             'name': "Ken",
             'age': 34,
             'email': "11111@gmail.com",
             'password': "000"
         };
-        const body = await agent.post("/users").send(userObj);
+        const res = await agent.post("/users").send(userObj);
         expect(StatusCodes.BAD_REQUEST);
-        expect(body.text).toEqual("{\"errors\":[],\"message\":\"User with such email has already existed\"}")
+        expect(res.text).toEqual("{\"errors\":[],\"message\":\"User with such email has already existed\"}");
+        expect(res).toHaveProperty('req');
+    });
+
+    it("GET /users - success", async () => {
+        const res = await agent.get("/users");
+        expect(StatusCodes.OK);
+        let firstUser = JSON.parse(res.text).data.users[0];
+        expect(firstUser.id).toEqual(1);
     })
 });
